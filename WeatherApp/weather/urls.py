f5da8575fc63302d63bc3d8acb8d600d66c1ad83
Bauyrzhan Zhanuzakov <83038196+ZhanB-Z@users.noreply.github.com>
2022-11-22 17:25:12 +0100

    Update urls.py

--- WeatherApp/weather/urls.py
+++ WeatherApp/weather/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, )
+    path('', views.home, name='home' ),
+    path('error/', views.error, name='error'),
+    path('delete/<str:pk>/', views.deleteCity, name='delete')
 ]
