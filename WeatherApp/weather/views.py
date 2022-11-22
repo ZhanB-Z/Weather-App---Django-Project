92684cd100bc2c87f15fc28a7f6dfda17e64470f
Bauyrzhan Zhanuzakov <83038196+ZhanB-Z@users.noreply.github.com>
2022-11-22 17:24:10 +0100

    Update views.py

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,37 +1,70 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-def index(request):
+def home(request):
     appid = '2972edf4bc2a4121567fb4a5e4e3188a'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     all_cities = []
 
-    if request.method == "POST":
-        form = CityForm(request.POST)
-        form.save()
-
-    form = CityForm() # this line will clean up the city form and keep it empty after page is updated
-
-
     cities = City.objects.all()
+
+    if request.method == "POST":
+        try:
+            form = CityForm(request.POST)
+            # i need to check here weather citi is available on the web-page
+            if form.is_valid:
+                form.save()
+
+        except:
+            return redirect('error')
+
+    form = CityForm()
+
     for city in cities:
         res = requests.get(url.format(city.name)).json()
-        print(res)
 
-        city_info = {
+        # print(res)
+        try:
+            city_info = {
+                'id': city.id,
                 'city': city.name,
                 'temp': res["main"]["temp"],
+                'feels_like': res["main"]["feels_like"],
+                'humidity': res["main"]["humidity"],
                 'icon': res["weather"][0]["icon"]}
-        all_cities.append(city_info)
+
+            all_cities.append(city_info)
+
+        except:
+            City.objects.filter(name=city).delete()
+            return redirect('error')
+
+    print(all_cities)
 
     context = {'all_info': all_cities, 'form': form}
 
-    return render(request, 'weather/index.html', context)
+    return render(request, 'weather/home.html', context)
 
+
+def error(request):
+
+    return render(request, 'weather/error.html')
+
+
+def deleteCity(request, pk):
+    city = City.objects.get(name=pk)  
+
+    if request.method == "POST":
+        # print(city)
+        city.delete()
+        return redirect('home')
+
+    return render(request, 'weather/delete.html')
 
